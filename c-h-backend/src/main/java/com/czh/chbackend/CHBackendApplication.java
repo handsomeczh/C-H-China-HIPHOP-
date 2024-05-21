9e59596bb0e34f9c0d6bc340f5e4b25695fc7441
chenzehan <282567367@qq.com>
2024-05-21 17:19:33 +0800

    后端项目初始化

---
+++ c-h-backend/src/main/java/com/czh/chbackend/CHBackendApplication.java
@@ -0,0 +1,13 @@
+package com.czh.chbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CHBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CHBackendApplication.class, args);
+    }
+
+}
---
+++ c-h-backend/src/test/java/com/czh/chbackend/CHBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czh.chbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CHBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
